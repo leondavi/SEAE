3e4ecb059c007ab1b3e42ad635beccb67d03d141
david <ddleon@outlook.co.il>
2019-07-07 15:21:52 +0300

    starting

---
+++ main.py
@@ -0,0 +1,4 @@
+import networkx as nx
+
+
+G = nx.random_regular_graph(6,100)
---
+++ spectral_clusttering.py
@@ -0,0 +1,14 @@
+import torch
+import networkx as nx
+
+
+def spectral_clusttering(S,k):
+
+
+"""
+S - Input Similarity Matrix
+
+Return - Matrix of eigen vectors 
+"""
+def spectral_embedding(S):
+    
